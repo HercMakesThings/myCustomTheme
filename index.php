9b0d24534f83031fae63d6bdf4bdc5efc639c43d
symphorian <86629083+HercMakesThings@users.noreply.github.com>
2022-05-04 12:46:54 -0400

    1st commit

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alumni Designs</title>
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="myassets/bootstrap/css/bootstrap.css">
+    <script src="myassets/bootstrap/js/bootstrap.js" type="text/javascript"></script>
+    <!--AOSJS-->
+    <link rel="stylesheet" href="myassets/aos-master/dist/aos.css">
+    <script type="text/javascript" src="myassets/aos-master/dist/aos.js"></script>
+    <!--Font Awesome CDN-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <!--Jquery-->
+    <script type="text/javascript" src="myassets/js/jquery-3.6.0.js"></script>
+    <!--Roboto Font (Google)-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <!--Personal Stylesheet-->
+    <link rel="stylesheet" href="myassets/css/alum.css">
+  </head>
+  <body>
+    <!-- test -->
+    <!--homepagesection start-->
+    <section id="homepage">
+    <!--landingPage Top Section-->
+    <section id="topsec" class="scrolldemo">
+      <div data-aos="zoom-in-right" id="top-title" class="row align-items-center justify-items-center">
+        <img class="col-sm-6" src="myassets/media/placeholder1.png" alt="">
+        <h1 class="col-sm-6">Alumni Designs</h1>
+      </div>
+        <a id="scrollbtn1" href="#topnavbar"><!--<span class="far fa-arrow-alt-circle-down fa-2x"></span>--><span></span></a>
+    </section>
+    <!--landingPage Top Section END-->
+
+    <!--Top Level Navigation-->
+    <div id="topnavbarcontainer" class="container-fluid">
+      <nav id="topnavbar" class="nav topnav justify-content-end">
+        <a id="home-link" class="nav-link childclear" href="index.html">Home</a>
+        <a id="portfolio-link" class="nav-link childclear" href="page.html">Portfolio</a>
+        <a id="contact-link" class="nav-link childclear" href="page.html">Contact</a>
+        <a id="blog-link" class="nav-link childclear" href="blog.html">Blog</a>
+        <a class="nav-link icon" href="javascript:void(0);">
+            <i class="fas fa-bars"></i>
+        </a>
+      </nav>
+    </div>
+
+    <!--Slanted Sections-->
+    <div id="slantseccontainer" class="container-fluid">
+
+
+        <section id="slantedsec1" class="row gx-0 align-items-center justify-content-center">
+            <div data-aos="fade-in-right" id="aboutcol1" class="col-sm-7 p-4">
+              <p>INSERT ABOUT ME POST CONTENT HERE Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <div data-aos-duration="1500" data-aos="fade-right" class="accentlineleft"></div>
+            </div>
+            <!--<div class="vl col-sm-1"></div>-->
+            <div data-aos="fade-in-right" id="aboutcol2" class="col-sm-4">
+              <h1>About Me</h1>
+              <small>Insert about post title hook here</small>
+              <div data-aos-duration="1500" data-aos="zoom-in" class="accentlinecenter"></div>
+            </div>
+        </section>
+
+        <section id="slantedsec2" class="row g-0 align-items-center justify-content-center">
+          <div data-aos="fade-in-right" id="skillscol1" class="col-sm-5">
+            <h1>Skills</h1>
+            <!--<span class="fas fa-arrow-alt-from-left"></span>-->
+            <small>Place the skills post title hook here</small>
+            <div data-aos-duration="2000" data-aos="zoom-in" class="accentlinecenterdark"></div>
+          </div>
+          <div data-aos="fade-in-right" id="skillscol2" class="col-sm-6">
+            <ul>
+              <li>Skill list/bars go here</li>
+              <li>Probably through use of a wp plugin</li>
+              <li>list item 3</li>
+              <li>list item 4</li>
+            </ul>
+            <div data-aos-duration="2000" data-aos="fade-right" class="accentlineleftdark"></div>
+          </div>
+        </section>
+
+    </div><!--SlantedSecContainersEND-->
+
+
+
+    <footer class="container-fluid">
+      <div class="container">
+        <div class="row justify-content-center">
+            <div data-aos="flip-down" class="col-sm-5 mt-3">
+              <div class="card">
+                <div class="card-header">See My Work!</div>
+                <div class="card-body">
+                  <nav>
+                    <ul>
+                      <li><a href="index.html">Home</a></li>
+                      <li><a href="page.html">Portfolio</a></li>
+                      <li><a href="page.html">Contact</a></li>
+                    </ul>
+                  </nav>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-5 mt-3">
+              <div data-aos="flip-up" class="card">
+                <div class="card-header">Social Media</div>
+                <div class="card-body">
+                  <ul>
+                    <li><a href="https://twitter.com"><i class="fab fa-twitter"></i> Twitter</a></li>
+                    <li><a href="https://www.facebook.com"><i class="fab fa-facebook"></i> Facebook</a></li>
+                    <li><a href="https://github.com"><i class="fab fa-github"></i> Github</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+    </footer>
+
+  </section><!--homepagesection END-->
+
+    <script type="text/javascript" src="myassets/js/alum.js"></script>
+  </body>
+</html>
